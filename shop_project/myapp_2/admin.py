8a4f157e9ffeea00ca38eb847206265a9edc3522
NIKITA <kit.zaver@yandex.ru>
2024-02-29 23:28:07 +0300

    Home work 5

--- shop_project/myapp_2/admin.py
+++ shop_project/myapp_2/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Client, Product, Order
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone_number',]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['get_client_name', 'get_products_list', 'total_amount', 'order_date']
+
+    def get_products_list(self, obj):
+        return ", ".join([product.name for product in obj.products.all()])
+    get_products_list.short_description = 'Продукция'
+
+    def get_client_name(self, obj):
+        return obj.client.name
+    get_client_name.short_description = 'Имя клиента'
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
